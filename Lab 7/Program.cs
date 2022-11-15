de13f8be75327bc7150ef304858a55f8f8d8fb00
Ilya Markov <imarkovkin@gmail.com>
2022-11-16 00:25:13 +0300

    Добавьте файлы проекта.

---
+++ Lab 7/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7
+{
+    class Program
+    {
+        static double GetPerimTriangle (double a, double b, double c, out double s)
+        {
+            double p = (a + b + c) / 2;
+            return s = Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите стороны первого треугольника");
+            double a1 = Convert.ToDouble(Console.ReadLine());
+            double b1 = Convert.ToDouble(Console.ReadLine());
+            double c1 = Convert.ToDouble(Console.ReadLine());
+            double s;
+            double s1 = GetPerimTriangle(a1, b1, c1, out s);
+            Console.WriteLine("{0}", s1);
+            Console.WriteLine("введите стороны второго треугольника");
+            double a2 = Convert.ToDouble(Console.ReadLine());
+            double b2 = Convert.ToDouble(Console.ReadLine());
+            double c2 = Convert.ToDouble(Console.ReadLine());
+            double s2 = GetPerimTriangle(a2, b2, c2, out s);
+            Console.WriteLine("{0}", s2);
+            if (s1>s2)
+            {
+                Console.WriteLine("площадь первого треугольника больше площади второго");
+            }
+            else
+            {
+                if (s1 < s2)
+                {
+                    Console.WriteLine("площадь первого треугольника меньше площади второго");
+                }
+                else
+                {
+                    Console.WriteLine("площади равны");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void GetCubeParams(double a, out double s, out double v)
+        {
+            s = 6*a*a;
+            v = a * a * a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите сторону куба");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double s;
+            double v;
+            GetCubeParams(a, out s, out v);
+            Console.WriteLine("Площадь поверхности куба - {0:.00}, Объем куба - {1:.00}", s, v);
+            Console.ReadKey();
+        }
+    }
+}
